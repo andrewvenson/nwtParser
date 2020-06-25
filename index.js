1cac80e0f2d461777ffc115e077dd7d4afc0352b
andrewvenson <vensoneandrew@gmail.com>
2020-06-25 17:54:25 -0500

    biblical parse

---
+++ index.js
@@ -0,0 +1,45 @@
+const genesis2 = `2  Thus the heavens and the earth and everything in them* were completed.a 2  And by the seventh day, God had completed the work that he had been doing,* and he began to rest on the seventh day from all his work that he had been doing.*b 3  And God went on to bless the seventh day and to declare it sacred, for on it God has been resting from all the work that he has created, all that he purposed to make.
+4  This is a history of the heavens and the earth in the time they were created, in the day that Jehovah* God made earth and heaven.c
+5  No bush of the field was yet on the earth and no vegetation of the field had begun sprouting, because Jehovah God had not made it rain on the earth and there was no man to cultivate the ground. 6  But a mist would go up from the earth, and it watered the entire surface of the ground.
+7  And Jehovah God went on to form the man out of dustd from the ground and to blow into his nostrils the breath of life,e and the man became a living person.*f 8  Further, Jehovah God planted a garden in Eʹden,g toward the east; and there he put the man whom he had formed.h 9  Thus Jehovah God made to grow out of the ground every tree that was pleasing to look at and good for food and also the tree of lifei in the middle of the garden and the tree of the knowledge of good and bad.j
+10  Now there was a river flowing out of Eʹden to water the garden, and from there it divided into four rivers.* 11  The name of the first is Piʹshon; it is the one encircling the entire land of Havʹi·lah, where there is gold. 12  The gold of that land is good. Bdellium gum and onyx stone are also there. 13  The name of the second river is Giʹhon; it is the one encircling the entire land of Cush. 14  The name of the third river is Hidʹde·kel;*k it is the one going to the east of As·syrʹi·a.l And the fourth river is the Eu·phraʹtes.m
+15  Jehovah God took the man and settled him in the garden of Eʹden to cultivate it and to take care of it.n 16  Jehovah God also gave this command to the man: “From every tree of the garden you may eat to satisfaction.o 17  But as for the tree of the knowledge of good and bad, you must not eat from it, for in the day you eat from it you will certainly die.”p
+18  Then Jehovah God said: “It is not good for the man to continue to be alone. I am going to make a helper for him, as a complement of him.”q 19  Now Jehovah God had been forming from the ground every wild animal of the field and every flying creature of the heavens, and he began bringing them to the man to see what he would call each one; and whatever the man would call each living creature,* that became its name.r 20  So the man named all the domestic animals and the flying creatures of the heavens and every wild animal of the field, but for man there was no helper as a complement of him. 21  So Jehovah God caused the man to fall into a deep sleep, and while he was sleeping, he took one of his ribs and then closed up the flesh over its place. 22  And Jehovah God built the rib that he had taken from the man into a woman, and he brought her to the man.s
+23  Then the man said:
+“This is at last bone of my bones
+And flesh of my flesh.
+This one will be called Woman,
+Because from man she was taken.”t
+24  That is why a man will leave his father and his mother and he will stick to* his wife, and they will become one flesh.u 25  And both of them continued to be naked,v the man and his wife; yet they were not ashamed.`;
+
+var genesis1 = `1  In the beginning God created the heavens and the earth.a
+2  Now the earth was formless and desolate,* and there was darkness upon the surface of the watery deep,*b and God’s active force*c was moving about over the surface of the waters.d
+3  And God said: “Let there be light.” Then there was light.e 4  After that God saw that the light was good, and God began to divide the light from the darkness. 5  God called the light Day, but the darkness he called Night.f And there was evening and there was morning, a first day.
+6  Then God said: “Let there be an expanseg between the waters, and let there be a division between the waters and the waters.”h 7  Then God went on to make the expanse and divided the waters beneath the expanse from the waters above the expanse.i And it was so. 8  God called the expanse Heaven.* And there was evening and there was morning, a second day.
+9  Then God said: “Let the waters under the heavens be collected together into one place, and let the dry land appear.”j And it was so. 10  God called the dry land Earth,k but the collecting of the waters, he called Seas.l And God saw that it was good.m 11  Then God said: “Let the earth cause grass to sprout, seed-bearing plants and fruit trees according to their kinds, yielding fruit along with seed on the earth.” And it was so. 12  And the earth began to produce grass, seed-bearing plantsn and trees yielding fruit along with seed, according to their kinds. Then God saw that it was good. 13  And there was evening and there was morning, a third day.
+14  Then God said: “Let there be luminaries*o in the expanse of the heavens to make a division between the day and the night,p and they will serve as signs for seasons and for days and years.q 15  They will serve as luminaries in the expanse of the heavens to shine upon the earth.” And it was so. 16  And God went on to make the two great luminaries, the greater luminary for dominating the dayr and the lesser luminary for dominating the night, and also the stars.s 17  Thus God put them in the expanse of the heavens to shine upon the earth 18  and to dominate by day and by night and to make a division between the light and the darkness.t Then God saw that it was good. 19  And there was evening and there was morning, a fourth day.
+20  Then God said: “Let the waters swarm with living creatures,* and let flying creatures fly above the earth across the expanse of the heavens.”*u 21  And God created the great sea creatures* and all living creatures* that move and swarm in the waters according to their kinds and every winged flying creature according to its kind. And God saw that it was good. 22  With that God blessed them, saying: “Be fruitful and become many and fill the waters of the sea,v and let the flying creatures become many in the earth.” 23  And there was evening and there was morning, a fifth day.
+24  Then God said: “Let the earth bring forth living creatures* according to their kinds, domestic animals and creeping animals* and wild animals of the earth according to their kinds.”w And it was so. 25  And God went on to make the wild animals of the earth according to their kinds and the domestic animals according to their kinds and all the creeping animals of the ground according to their kinds. And God saw that it was good.
+26  Then God said: “Let usx make man in our image,y according to our likeness,z and let them have in subjection the fish of the sea and the flying creatures of the heavens and the domestic animals and all the earth and every creeping animal that is moving on the earth.”a 27  And God went on to create the man in his image, in God’s image he created him; male and female he created them.b 28  Further, God blessed them, and God said to them: “Be fruitful and become many, fill the earthc and subdue it,d and have in subjectione the fish of the sea and the flying creatures of the heavens and every living creature that is moving on the earth.”
+29  Then God said: “Here I have given to you every seed-bearing plant that is on the entire earth and every tree with seed-bearing fruit. Let them serve as food for you.f 30  And to every wild animal of the earth and to every flying creature of the heavens and to everything moving on the earth in which there is life,* I have given all green vegetation for food.”g And it was so.
+31  After that God saw everything he had made, and look! it was very good.h And there was evening and there was morning, a sixth day.`;
+
+function parseChapter(chapter) {
+  const re = /[0-9]+[ ]+/;
+  let arr = chapter.split(re);
+  let parsed = arr.map((verse, index) => {
+    let v = verse;
+    v = v.replace(/\*/g, "");
+    v = v.replace(/\n/g, "");
+    v = v.replace(/[.][a-z][ ]+/g, ".");
+    v = v.replace(/[.][ ]+/g, ".");
+    v = v.replace(/[.][a-z]/g, ".");
+    v = v.replace(/[,][a-z]/g, ",");
+    v = v.replace(/[.]”[a-z][ ]*/g, ".”");
+
+    return v;
+  });
+  console.log(Object.assign({}, parsed));
+}
+
+parseChapter(genesis2);
