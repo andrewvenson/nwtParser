0fa6250ed31b0db063f4bb4d161b1a93e446eda9
andrewvenson <vensoneandrew@gmail.com>
2020-06-26 09:19:42 -0500

    initial commit to gh, function created to parse chapters from nwt

--- index.js
+++ index.js
@@ -24,6 +24,169 @@ var genesis1 = `1  In the beginning God created the heavens and the earth.a
 29  Then God said: “Here I have given to you every seed-bearing plant that is on the entire earth and every tree with seed-bearing fruit. Let them serve as food for you.f 30  And to every wild animal of the earth and to every flying creature of the heavens and to everything moving on the earth in which there is life,* I have given all green vegetation for food.”g And it was so.
 31  After that God saw everything he had made, and look! it was very good.h And there was evening and there was morning, a sixth day.`;
 
+var psalms78 = `78  Listen, my people, to my law;*
+Incline your ear to the words of my mouth.
+ 2  I will open my mouth in a proverb.
+I will propound riddles of long ago.b
+ 3  The things we have heard and know,
+Which our fathers have related to us,c
+ 4  We will not hide from their sons;
+We will relate to the generation to comed
+The praiseworthy deeds of Jehovah and his strength,e
+The wonderful things he has done.f
+ 5  He established a reminder in Jacob
+And set a law in Israel;
+He commanded our forefathers
+To make these things known to their children,g
+ 6  So that the next generation,
+The children yet to be born, might know them.h
+They in turn would relate them to their children.i
+ 7  Then these would put their confidence in God.
+They would not forget God’s worksj
+But would observe his commandments.k
+ 8  Then they would not be like their forefathers,
+A stubborn and rebellious generation,l
+A generation whose heart was unsteady*m
+And whose spirit was not faithful to God.
+ 9  The Eʹphra·im·ites were armed with the bow,
+But they retreated in the day of battle.
+10  They did not keep the covenant of Godn
+And refused to walk in his law.o
+11  They also forgot what he had done,p
+His wonderful works that he showed them.q
+12  He did marvelous things in the sight of their forefathers,r
+In the land of Egypt, the region of Zoʹan.s
+13  He split the sea to let them pass through,
+And he made the waters stand up like a dam.*t
+14  He led them with a cloud by day
+And the whole night with the light of a fire.u
+15  He split rocks in the wilderness,
+He let them drink their fill as if from deep waters.v
+16  He brought streams out of a crag
+And caused waters to flow down like rivers.w
+17  But they continued to sin against him
+By rebelling against the Most High in the desert;x
+18  They challenged* God in their hearty
+By demanding food that they craved.*
+19  So they spoke against God,
+Saying: “Can God spread a table in the wilderness?”z
+20  Look! He struck a rock
+So that waters flowed and streams flooded out.a
+“Can he also give us bread,
+Or can he provide meat for his people?”b
+21  When Jehovah heard them, he became furious;c
+A fired blazed against Jacob,
+And his anger flared up against Israele
+22  Because they did not put faith in God;f
+They did not trust in his ability to save them.
+23  So he commanded the cloudy skies above,
+And he opened the doors of heaven.
+24  He kept raining down manna for them to eat;
+He gave them the grain of heaven.g
+25  Men ate the bread of mighty ones;*h
+He provided enough to satisfy them.i
+26  He stirred up the east wind in the heavens
+And made a south wind blow by his power.j
+27  And he rained down meat on them like dust,
+Birds like the sand of the seashore.
+28  He made them fall in the midst of his camp,
+All around his tents.
+29  And they ate and gorged themselves;
+He gave them what they desired.k
+30  But before they fully satisfied their craving,
+While their food was yet in their mouth,
+31  God’s wrath flared up against them.l
+He put to death their strongest men;m
+He brought down the young men of Israel.
+32  Despite this they sinned even moren
+And did not put faith in his wonderful works.o
+33  So he brought their days to an end as if a mere breath,p
+And their years by sudden terrors.
+34  But as often as he killed them, they would search for him;q
+They would return and look for God,
+35  Remembering that God was their Rockr
+And that the Most High God was their Redeemer.*s
+36  But they tried to deceive him with their mouth
+And lied to him with their tongue.
+37  Their heart was not steadfast toward him;t
+And they were not faithful to his covenant.u
+38  But he was merciful;v
+He would forgive* their error and not bring them to ruin.w
+He often held back his angerx
+Instead of stirring up all his wrath.
+39  For he remembered that they were flesh,y
+A wind that blows past and does not return.*
+40  How often they rebelled against him in the wildernessz
+And made him feel hurt in the desert!a
+41  Again and again they put God to the test,b
+And they grieved* the Holy One of Israel.
+42  They did not remember his power,*
+The day that he rescued* them from the adversary,c
+43  How he displayed his signs in Egyptd
+And his miracles in the region of Zoʹan,
+44  And how he turned the Nile canals into bloode
+So that they could not drink from their streams.
+45  He sent swarms of gadflies to devour themf
+And frogs to bring them to ruin.g
+46  He gave their crops to the voracious locusts,
+The fruit of their labor to the swarming locusts.h
+47  He destroyed their vines with haili
+And their sycamore trees with hailstones.
+48  He gave their beasts of burden over to the hailj
+And their livestock to the lightning bolts.*
+49  He inflicted his burning anger on them,
+Fury and indignation and distress,
+Companies of angels bringing calamity.
+50  He cleared a pathway for his anger.
+He did not spare them* from death;
+And he gave them* over to pestilence.
+51  Finally he struck down all the firstborn of Egypt,k
+The beginning of their procreative power in the tents of Ham.
+52  Then he brought his people out like a flockl
+And guided them like a drove in the wilderness.
+53  He led them in security,
+And they felt no fear;m
+The sea covered their enemies.n
+54  And he brought them to his holy territory,o
+This mountainous region that his right hand acquired.p
+55  He drove out the nations from before them;q
+He allotted to them an inheritance by the measuring line;r
+He settled the tribes of Israel in their homes.s
+56  But they kept challenging* God the Most High, and rebelling against him;t
+They did not pay attention to his reminders.u
+57  They also turned away and were as treacherous as their forefathers.v
+They were as unreliable as a loose bow.w
+58  They kept offending him with their high places,x
+And they incited him to fury* with their carved images.y
+59  God heard and became furious,z
+So he utterly rejected Israel.
+60  He finally forsook the tabernacle of Shiʹloh,a
+The tent where he had resided among men.b
+61  He let the symbol of his strength go into captivity;
+His splendor into the hand of the adversary.c
+62  He handed his people over to the swordd
+And became furious with his inheritance.
+63  Fire consumed his young men,
+And to his virgins no wedding songs were sung.*
+64  His priests fell by the sword,e
+And their own widows did not weep.f
+65  Then Jehovah woke up as from sleep,g
+Like a mighty manh waking up from wine.
+66  And he drove his adversaries back;i
+He put them to lasting disgrace.
+67  He rejected the tent of Joseph;
+He did not choose the tribe of Eʹphra·im.
+68  But he chose the tribe of Judah,j
+Mount Zion, which he loves.k
+69  He made his sanctuary as enduring as the heavens,*l
+Like the earth that he has established forever.m
+70  He chose Davidn his servant
+And took him from the sheep pens,o
+71  From tending the nursing ewes;
+He made him the shepherd over Jacob, his people,p
+And over Israel, his inheritance.q
+72  He shepherded them with integrity of heart,r
+And with skillful hands he led them.s`;
 function parseChapter(chapter) {
   const re = /[0-9]+[ ]+/;
   let arr = chapter.split(re);
@@ -31,15 +194,17 @@ function parseChapter(chapter) {
     let v = verse;
     v = v.replace(/\*/g, "");
     v = v.replace(/\n/g, "");
-    v = v.replace(/[.][a-z][ ]+/g, ".");
-    v = v.replace(/[.][ ]+/g, ".");
-    v = v.replace(/[.][a-z]/g, ".");
-    v = v.replace(/[,][a-z]/g, ",");
-    v = v.replace(/[.]”[a-z][ ]*/g, ".”");
+    v = v.replace(/[.][a-z][ ]+/g, ". ");
+    v = v.replace(/[;][a-z]/g, ";");
+    v = v.replace(/[;]/g, "; ");
 
-    return v;
+    v = v.replace(/[.][a-z]/g, ". ");
+    v = v.replace(/[,][a-z]/g, ", ");
+    v = v.replace(/.”[a-z][ ]*/g, ".”");
+
+    return v.trim();
   });
   console.log(Object.assign({}, parsed));
 }
 
-parseChapter(genesis2);
+parseChapter(psalms78);
